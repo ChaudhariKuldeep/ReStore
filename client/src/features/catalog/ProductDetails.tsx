dd4e67fe9cab839a75e52e2b8ec8086e93535657
chaudkul <kuldeep.chaudhari@tietoevry.com>
2022-07-26 18:16:10 +0530

    End of Section 5

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -8,25 +8,25 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 
 export default function ProductDetails() {
-  const { id } = useParams<{ id: string }>();
+  const {id} = useParams<{id: string}>();
   const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:5251/api/products/${id}`)
-      .then((response) => setProduct(response.data))
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false));
-  }, []);
+    agent.Catalog.details(parseInt(id!))
+            .then(response => setProduct(response))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+    }, [id]);
 
-  if (loading) return <h3>Loading...</h3>;
+  if (loading) return <LoadingComponent message='Loading Product' />;
 
   if (!product) return <h3>Product not found</h3>;
 
